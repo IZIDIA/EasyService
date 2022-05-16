45f30029179a8665f57f3f9746dad99a5c26fc6f
IZIDIA <ctalker-n@mail.ru>
2022-05-16 15:12:55 +0500

    fix `InstalledPrograms` list

--- PcInfo/InstalledPrograms.cs
+++ PcInfo/InstalledPrograms.cs
@@ -7,22 +7,40 @@ using System.Threading.Tasks;
 
 namespace EasyService.PcInfo {
 	public class InstalledPrograms {
+		const string Registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
 		public List<string> InstalledProgramsList { get; set; }
 		public InstalledPrograms() {
-			GetInstalledPrograms();
+			InstalledProgramsList = GetInstalledPrograms();
 		}
-		private void GetInstalledPrograms() {
-			InstalledProgramsList = new List<string>();
-			using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")) {
+		public static List<string> GetInstalledPrograms() {
+			var result = new List<string>();
+			result.AddRange(GetInstalledProgramsFromRegistry(RegistryView.Registry32));
+			result.AddRange(GetInstalledProgramsFromRegistry(RegistryView.Registry64));
+			return result;
+		}
+		private static IEnumerable<string> GetInstalledProgramsFromRegistry(RegistryView registryView) {
+			var result = new List<string>();
+			using (RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView).OpenSubKey(Registry_key)) {
 				foreach (string subkey_name in key.GetSubKeyNames()) {
 					using (RegistryKey subkey = key.OpenSubKey(subkey_name)) {
-						var value = subkey.GetValue("DisplayName");
-						if (value != null) {
-							InstalledProgramsList.Add(string.Format("{0}", value));
+						if (IsProgramVisible(subkey)) {
+							result.Add((string)subkey.GetValue("DisplayName"));
 						}
 					}
 				}
 			}
+			return result;
+		}
+		private static bool IsProgramVisible(RegistryKey subkey) {
+			var name = (string)subkey.GetValue("DisplayName");
+			var releaseType = (string)subkey.GetValue("ReleaseType");
+			var systemComponent = subkey.GetValue("SystemComponent");
+			var parentName = (string)subkey.GetValue("ParentDisplayName");
+			return
+					!string.IsNullOrEmpty(name)
+					&& string.IsNullOrEmpty(releaseType)
+					&& string.IsNullOrEmpty(parentName)
+					&& (systemComponent == null);
 		}
 	}
 }
